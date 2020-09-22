58481a5aaa1c543d2394f20b6f8f237ae591b521
Radrex1005 <71077712+Radrex1005@users.noreply.github.com>
2020-09-22 17:32:03 +0530

    Update login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,20 +1,37 @@
-var provider = new firebase.auth.GoogleAuthProvider();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBGO2B6hQTK4K5OE5zrbJ02vjdi6f28Erk",
+    authDomain: "vgstproposals.firebaseapp.com",
+    databaseURL: "https://vgstproposals.firebaseio.com",
+    projectId: "vgstproposals",
+    storageBucket: "vgstproposals.appspot.com",
+    messagingSenderId: "552421108644",
+    appId: "1:552421108644:web:6e33c08cbc16366cdeef31",
+    measurementId: "G-0QNPLQVH6Y"
+  };
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+    
+    const auth = firebase.auth();
+	var provider = new firebase.auth.GoogleAuthProvider();
 var user;
-
-
-$(document).ready(function(){
-  $("#welcome").hide();
-});
-
-
-function print(){
+function google(){
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
     user = result.user;
-    console.log(user.displayName);
-    showWelcomeContainer();
+    console.log(user);
+    var messagesRef = firebase.database().ref(user.uid+'/users'); 
+    var newPrincipalInvestigator = messagesRef.push();
+    newPrincipalInvestigator.set(
+        {
+        Name:user.name,
+   
+        }
+    );
     // ...
   }).catch(function(error) {
     // Handle Errors here.
@@ -27,17 +44,80 @@ function print(){
     // ...
   });
 }
+	
+	function signUp(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+		
+		alert("Signed Up");
+	}
+	
+	
+	
+	function signIn(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+		
+		
+		
+		
+	}
+	
+	
+	function signOut(){
+		
+		auth.signOut();
+		alert("Signed Out");
+		
+	}
+	
+	
+	
+	auth.onAuthStateChanged(function(user){
+		
+		if(user){
+			
+			var email = user.email;
+			alert("Active User " + email);
+			
+			//Take user to a different or home page
 
-function showWelcomeContainer(){
-  $("#login").hide();
-  $("#welcome").show();
-  $("#welcomeText").html("Hello, " + user.displayName);
-};
+			//is signed in
+			
+		}else{
+			
+			alert("No Active User");
+			//no user is signed in
+		}
+		
+		
+		
+	});
 
-$(".dropdown").on("hide.bs.dropdown", function(event){
-  var text = $(event.relatedTarget).text(); // Get the text of the element
-  firebase.database().ref('Users/' + user.uid).set({
-    name: user.displayName,
-    email: user.email
-  });
-}); 
+
+	const signInWithGoogleButton = document.getElementById('signInWithGoogle');
+
+	const signInWithGoogle = () => {
+		const googleProvider = new firebase.auth.GoogleAuthProvider();
+	  
+		auth.signInWithPopup(googleProvider)
+		.then(() => {
+		  window.location.assign('./profile');
+		})
+		.catch(error => {
+		  console.error(error);
+		})
+	  }
+	  
+	  signInWithGoogleButton.addEventListener('click', signInWithGoogle);
+
+	
+
